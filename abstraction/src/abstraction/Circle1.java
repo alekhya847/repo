8fedcb4609615f7a4fbff97689e361dfa5956756
QAPLANET3 <QAPLANET3@QAPLANET3-PC>
2020-02-26 10:49:38 +0530

    this is abstraction

---
+++ abstraction/src/abstraction/Circle1.java
@@ -0,0 +1,9 @@
+package abstraction;
+
+public class Circle1 extends Shape{
+	
+void draw()
+{
+	System.out.println("drawing circle");
+}
+}
---
+++ abstraction/src/abstraction/Testabstraction.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public class Testabstraction {
+
+	public static void main(String[] args) {
+		Shape s;
+		s=new Circle1();
+		s.draw();
+		s=new Rectangle();
+		s.draw();
+
+	}
+
+}
